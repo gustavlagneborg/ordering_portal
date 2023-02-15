67f97e5e5e779808b28dbfd2dbb98b1188c4ea99
gustavlagneborg <gustavlagneborg@hotmail.com>
2023-02-15 16:49:42 +0100

    finalizing file structura change

--- orderingplatform/routes.py
+++
@@ -1,9 +0,0 @@
-"""API and website routes"""
-
-from app import app
-from flask import request, render_template, flash, redirect,url_for
-
-@app.route("/")
-def index():
-    #return render_template("base.html")
-    return "Hello world"
