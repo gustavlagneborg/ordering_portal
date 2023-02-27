dc366ca9e2eb20bad17a490d3d91620c6081ad7a
gustavlagneborg <gustavlagneborg@hotmail.com>
2023-02-27 13:30:08 +0100

    fix api and database rebase

--- project/__init__.py
+++ project/__init__.py
@@ -37,7 +37,7 @@ def create_app(config_filename=None):
     # Check if the database needs to be initialized
     engine = sa.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
     inspector = sa.inspect(engine)
-    if not inspector.has_table("users"):
+    if not inspector.has_table("user"):
         with app.app_context():
             db.drop_all()
             db.create_all()
--- project/api/__init__.py
+++ project/api/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-api_blueprint = Blueprint("api", __name__)
+api_blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 
 from . import routes
