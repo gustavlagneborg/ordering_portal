8767bb6f6de0fb7baada6ce3626f769837b08934
gustavlagneborg <gustavlagneborg@hotmail.com>
2023-03-03 12:51:56 +0100

    setting up database models

--- project/ordering_portal/models.py
+++ project/ordering_portal/models.py
@@ -7,7 +7,7 @@ from .constants import ModelConstants
 
 
 class User(UserMixin, db.Model):
-    """User model for the database."""
+    """User table."""
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
@@ -32,7 +32,7 @@ class User(UserMixin, db.Model):
 
 
 class Project(db.Model):
-    """Porject model for the database."""
+    """Porject table."""
 
     id = db.Column(db.Integer, primary_key=True)
     project_name = db.Column(db.String, nullable=False, index=True)
@@ -46,8 +46,75 @@ class Project(db.Model):
     )
     patient_sex = db.Column(types.Enum(ModelConstants.PATIENT_SEX), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    
 
 
+class Examination(db.Model):
+    """Examination table."""
+
+    pass
 
 
+class ProjectExaminations(db.Model):
+    """Association table between Project and Examination."""
+
+    pass
+
+
+class DataDelivery(db.Model):
+    """DataDelivery table."""
+
+    pass
+
+
+class ProjectDataDelivery(db.Model):
+    """Association table between Project and DataDelivery."""
+
+    pass
+
+
+class Modality(db.Model):
+    """Modality Table."""
+
+    pass
+
+
+class ProjectModalities(db.Model):
+    """Association table between Project and Modality."""
+
+    pass
+
+
+class Remittent(db.Model):
+    """Remittent table."""
+
+    pass
+
+
+class ProjectRemittances(db.Model):
+    """Association table between Project and Remittent."""
+
+    pass
+
+
+class Department(db.Model):
+    """Department table."""
+
+    pass
+
+
+class ProjectDepartments(db.Model):
+    """Association table between Project and Department."""
+
+    pass
+
+
+class Laboratory(db.Model):
+    """Laboratory table."""
+
+    pass
+
+
+class ProjectLaboratories(db.Model):
+    """Association table between Project and Department."""
+
+    pass
