f9e1111dae9946d7190d0afb34cb4ab93e4de186
gustavlagneborg <gustavlagneborg@hotmail.com>
2023-03-21 12:39:44 +0100

    fixing getProjects

--- project/static/api.js
+++ project/static/api.js
@@ -27,22 +27,33 @@ function login(username, password) {
       });
   }
 
-  async function getProject() {
-    let token = localStorage.getItem('token'); // get the token from localStorage (or wherever it's stored)
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNfaWQiOiJwdWJsaWMtaWQtYWRtaW4iLCJleHAiOjE2ODE5ODc3MTd9.tAwLitiryY6lA4XR76YYbF3ZpGbjci8WwsWeBf1MTd0";
-    // check if token is expired (assuming the token has an "exp" claim that specifies the expiration time)
-    const decodedToken = jwt_decode(token);
-    const expirationTime = decodedToken.exp * 1000; // convert expiration time to milliseconds
-    const isExpired = Date.now() >= expirationTime;
+  async function queryOrderingPortal() {
+    const username = 'API_Admin';
+    const password = 'apiadmin';
+    const token = await login(username, password);
+    return token;
+    // // let token = localStorage.getItem('token'); // get the token from localStorage (or wherever it's stored)
     
-    // if token is expired, refresh it with a new login request
-    if (isExpired) {
-      token = await doLogin(); // assuming you have a working doLogin function that returns a new token
-    }
+    // // check if token is expired (assuming the token has an "exp" claim that specifies the expiration time)
+    // const decodedToken = jwt_decode(token);
+    // const expirationTime = decodedToken.exp * 1000; // convert expiration time to milliseconds
+    // const isExpired = Date.now() >= expirationTime;
     
+    // // if token is expired, refresh it with a new login request
+    // if (isExpired) {
+    //     const token = await login(username, password);
+    //     console.log(token);
+    //     return token;
+    // }
+    
+  }
+
+  async function getProjects() {
+
     const url = 'http://127.0.0.1:5000/api/v1/projects'; // replace with your API endpoint URL
     
     const headers = new Headers();
+    const token = await queryOrderingPortal();
     headers.set('x-access-token', token); // set the JWT token in the x-access-token header
     
     const options = {
@@ -63,11 +74,8 @@ function login(username, password) {
       });
   }
 
-  async function doLogin() {
-    const username = 'API_Admin';
-    const password = 'apiadmin';
-    const token = await login(username, password);
-    console.log(token);
-  }
 
-  console.log(getProject());
+let projects = getProjects();
+projects.then(function(result) {
+    console.log(result) // "Some User token"
+ })
