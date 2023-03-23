fc3fd8d79c91396fade5af315511b7d1e186428f
gustavlagneborg <gustavlagneborg@hotmail.com>
2023-03-23 19:51:42 +0100

    moving script

--- project/static/script.js
+++ project/static/script.js
@@ -4,3 +4,25 @@ function createKibanaLink() {
     link.style.visibility = 'visible'
     link.style.display = "block"
 }
+
+function searchProjectTable() {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("searchProjectInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("project-table");
+        tr = table.getElementsByTagName("tr");
+
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
